a148cb4003e991a7531477552610f31de6f0f5b5
carlynorama <carlynorama@gmail.com>
2024-02-09 07:35:01 -0800

    Initial Commit

---
+++ Lines/LinesApp.swift
@@ -0,0 +1,17 @@
+//
+//  LinesApp.swift
+//  Lines
+//
+//  Created by Carlyn Maw on 2/9/24.
+//
+
+import SwiftUI
+
+@main
+struct LinesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
